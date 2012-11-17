112877abde58c25abe98e632203d1dc963628800
Christopher Vollick <psycotica0@gmail.com>
2012-11-17 15:00:14 -0500

    Changed State to GameState
    This object will pick up basically everything from now on.

--- water.hs
+++ water.hs
@@ -1,5 +1,5 @@
 import Graphics.UI.SDL.General (withInit, InitFlag(InitVideo, InitEventthread))
-import Graphics.UI.SDL.Types (Surface, SurfaceFlag(SWSurface), surfaceGetPixelFormat, surfaceGetHeight, surfaceGetWidth)
+import Graphics.UI.SDL.Types (Surface, SurfaceFlag(SWSurface), surfaceGetPixelFormat, surfaceGetHeight, surfaceGetWidth, PixelFormat)
 import Graphics.UI.SDL.Video (setVideoMode, mapRGB, fillRect)
 import Graphics.UI.SDL.Color (Pixel)
 import qualified Graphics.UI.SDL.Video as V (flip)
@@ -9,8 +9,14 @@ import Graphics.UI.SDL.Events (Event(Quit), waitEvent)
 
 import Graphics.UI.SDL.Rect (Rect(Rect))
 
-import Control.Monad.Trans.State.Lazy (StateT, runStateT, get, put)
+import Control.Monad.Trans.State.Lazy (StateT, runStateT)
 import Control.Monad.Trans.Class (lift)
+
+import Prelude hiding ((.))
+import Control.Category ((.))
+
+import Data.Lens.Common (lens)
+import Data.Lens.Lazy ((~=), access, (%=))
 
 tile_width = 16
 tile_height = 16
@@ -18,28 +24,42 @@ tile_height = 16
 x_tiles = 24
 y_tiles = 16
 
-sand_color :: StateT Surface IO Pixel
-sand_color = do
-	screen <- get
-	lift $ mapRGB (surfaceGetPixelFormat screen) 255 169 95
+data GameState = GameState Surface PixelFormat
+initial_game_state = GameState undefined undefined
 
-setup :: StateT Surface IO ()
+getScreen (GameState screen _) = screen
+setScreen screen (GameState _ format) = GameState screen format
+
+screen = lens getScreen setScreen
+
+getFormat (GameState _ format) = format
+setFormat format (GameState screen _) = GameState screen format
+
+format = lens getFormat setFormat
+
+sand_color :: StateT GameState IO Pixel
+sand_color = do
+	f <- access format
+	lift $ mapRGB f 255 169 95
+
+setup :: StateT GameState IO ()
 setup = do
-	screen <- lift $ setVideoMode (x_tiles * tile_width) (y_tiles * tile_height) 32 [SWSurface]
-	put screen
+	s <- lift $ setVideoMode (x_tiles * tile_width) (y_tiles * tile_height) 32 [SWSurface]
+	screen ~= s
+	format ~= surfaceGetPixelFormat s
 	color <- sand_color
-	lift $ fillRect screen (Just (Rect 0 0 (surfaceGetWidth screen) (surfaceGetHeight screen))) color
-	lift $ V.flip screen
+	lift $ fillRect s (Just (Rect 0 0 (surfaceGetWidth s) (surfaceGetHeight s))) color
+	lift $ V.flip s
 	return ()
 
-handle_event :: Event -> StateT Surface IO ()
+handle_event :: Event -> StateT GameState IO ()
 handle_event Quit = return ()
 handle_event _ = main_loop
 
-main_loop :: StateT Surface IO ()
+main_loop :: StateT GameState IO ()
 main_loop = do
 	-- Normally we'd do stuff here...
 	event <- lift waitEvent
 	handle_event event
 
-main = withInit [InitVideo, InitEventthread] $ (>>) (setCaption "Water Game" "") $ (flip runStateT) undefined (setup >> main_loop)
+main = withInit [InitVideo, InitEventthread] $ (>>) (setCaption "Water Game" "") $ (flip runStateT) initial_game_state (setup >> main_loop)
